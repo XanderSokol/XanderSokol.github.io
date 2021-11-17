5691b933f5226a0163013b41319ff30d372b6301
XanderSokol <xandersokol1@gmail.com>
2021-11-17 13:18:57 +0300

    Create front-page.php

---
+++ example/front-page.php
@@ -0,0 +1,215 @@
+<?php get_header(); ?>
+<style>
+	.form-foot{
+	padding: 0 5%;
+	}
+.form-foot label{
+  	font-weight: 600;
+  	text-shadow: 0px 0px #fff; /*1 1*/
+  	color: #000;
+  	width: 100%;
+  	margin: 5px 0;
+  	height: 30px;
+  	box-sizing:border-box;
+  	margin-bottom: 15px; 
+}
+.form-foot input {
+  	width:100%;
+	font-family: 'open sans';
+  	background: #f2f2f2;
+  	background: white;
+	height: 40px;
+    text-align: left;
+    color: #757575;
+	font-size:14px;	
+	border-radius: 5px;
+	}
+.form-foot input[type="submit"] {
+	color: #0c0705;
+	background: #ca9e33;
+	font-weight: 550;
+	font-size: 14px;
+	width: 100%;
+	margin: 5% 0;
+	padding: 0 0;
+	border-radius: 0;
+}
+
+.form-foot .wpcf7-response-output {
+	font-size:16px;
+	line-height:16px;
+}
+	
+@media (min-width: 768px) {
+	.mobile-video {
+	display:none;
+	}
+}
+
+ @media (max-width: 767px) {
+	.top-video-bg {
+    background-color: #fa0;
+	display:none;
+	}
+	
+	.mobile-video {
+		display:block;
+	}
+	
+	.mobile-top {
+    right: -3em;
+    min-width: 100%;
+    width: 100%;
+	}
+/*  поля на мобильном */
+  .top-form input {
+	margin: 0 5%;
+    display: block;
+    width: 90%;
+  }
+} 
+</style>
+
+<!-- SECTION 1 -->
+<!-- SECTION 1 -->
+                          
+<section class="sec1">                          
+    <div id="top" class="hero-section">
+        <div class="container"> 
+            <div class="header-text">
+                <h2 class="bold-text">we are</h2>
+                <p class="head-text">150 ladies health clubs</p>
+                <span class="and-icon">&</span>
+                <p class="head-text text-bottom">exercise machines <br> manufacturer </p>
+            </div>
+                <div class="form"><a class="popup-header popmake-898">Get special offer</a>
+                </div>
+
+        </div> 
+
+</div>
+</section> 
+
+<!-- SECTION 2 -->
+<!-- SECTION 2 -->
+
+<section class="sec2">
+
+<div class="container">
+
+    <div class="online-consult">
+<h2 class="header-text-online-consult">get your free online consultation</h2>
+        <?php echo do_shortcode('[contact-form-7 id="935" title="GET YOUR FREE CONSULTATION ONLINE"]'); ?> 
+
+    </div>
+
+</div>
+
+</section>
+
+
+
+<!-- SECTION 3 -->
+<!-- SECTION 3 -->
+
+<section class="sec3">
+<!-- BLOCK 2 -->
+<div id="front-info" class="front-section">
+	<div class="container">
+		<div class="items-wrapper row">
+			<div class="item-frontpage col-sm-6 col-xs-12">
+				<div class="">
+					<a href="/toning-tables-all/">
+					<div class="figure-holder first-ch">
+                        <div class="img-overlay">
+						<img class="item-frontpage-img" src="wp-content/themes/entonus/assets/images/front-page/tt-model.jpg" alt="image"></div>
+					</div>
+					</a>
+					<div class="frontpage-desc">
+					<h2 class="item-title title-first">Toning tables</h2>
+						<p class="custom-item-first">The set of toning tables is suitable for using at sports and recreation facilities and is designed for sports, health improving and rehabilitation procedures, as well as passive-active gymnastics in the sitting and lying positions. Exercising on the toning tables contributes to the restoration of motor functions, strengthens muscles, permits to normalize weight and build a slim figure without exhausting workouts.</p>
+					</div>
+				</div>
+			</div>
+			<div class="item-frontpage col-sm-6 col-xs-12">
+				<div class="">
+					<a href="/exclusive-treadmill-machine">
+					<div class="figure-holder sec-ch">
+                         <div class="img-overlay">
+						<img class="item-frontpage-img" src="wp-content/themes/entonus/assets/images/front-page/tread-model.jpg" alt="image"></div>
+					</div>
+					</a>
+					<div class="frontpage-desc">
+					<h2 class="item-title title-second">Exclusive Treadmill Machine</h2>
+						<p class="custom-item-second">Exclusive Treadmill Machine is good for people, who want to make faster the body fat burning in their "problem areas".</p>
+					</div>
+				</div>
+			</div>
+			<div class="item-frontpage col-sm-6 col-xs-12">
+				<div class="">
+					<a href="/roller-massager-roller-massage-equipment-line-professional">
+					<div class="figure-holder first-ch">
+                         <div class="img-overlay">
+						<img class="item-frontpage-img" src="wp-content/themes/entonus/assets/images/front-page/beat-model.jpg" alt="image"></div>
+					</div>
+					</a>
+					<div class="frontpage-desc desc-third">
+					<h2 class="item-title title-third">'BeautyRoll' Exercise Machine<sup>®</sup></h2>
+						<p>The BeautyRoll Prof professional rolling exercise machine  is suitable for using at sports and health facilities, and is designed for health and preventive lymphatic drainage massage of legs, arms, buttocks, neck, abdomen and waist as well as other parts of the body.</p>
+					</div>
+				</div>
+			</div>
+			<div class="item-frontpage col-sm-6 col-xs-12">
+				<div class="">
+					<a href="/balance-platform-2">
+					<div class="figure-holder sec-ch">
+                           <div class="img-overlay">
+						<img class="item-frontpage-img" src="wp-content/themes/entonus/assets/images/front-page/balance-model.jpg" alt="image"></div>
+					</div>
+					</a>
+					<div class="frontpage-desc desc-four">
+					<h2 class="item-title title-fourth">Balance platform</h2>
+						<p class="custom-item-fourth">Balance platforms are a simple, effective way to train dynamic stability. Using the balance platform, you can significantly improve coordination as well as effectively work out deep and hard-to-reach muscle groups.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</section>
+
+<!-- SECTION 4 -->
+
+<section class="sec4">
+    <div class="container">
+
+        <div class="walk-through">
+
+            <iframe name = "myframe" width = "100%" height = "450px" src = "https://my.matterport.com/show/?m=cerdp1Gsxkc&ts=0&lang=en&play=1&lp=1&tourcta=0"></iframe>
+
+    </div>
+
+        </div>
+</section>
+
+<!-- SECTION 5 -->
+
+<section class="sec5">
+
+
+    <div class="container">
+        
+        <div class="last-form">
+                <h2 class="last-form-text">Open wellness club</h2>
+        <?php echo do_shortcode('[contact-form-7 id="685" title="Open welness club"]'); ?> 
+            
+            
+        </div>
+    </div>
+
+
+</section>
+  
+<?php get_footer(); ?>
+		
