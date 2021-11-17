1a5b089787a791783bc051f86e701ff2e1cc0b6b
XanderSokol <xandersokol1@gmail.com>
2021-11-17 13:21:52 +0300

    Create partner-page.php

---
+++ example/partner-page.php
@@ -0,0 +1,560 @@
+<?php
+/*
+Template Name: Partner Page
+*/
+?>
+
+    <title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <!-- Meta -->
+    <meta name="yandex-verification" content="48ccddea3086038b" />
+    <meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Manufacture and distributor of exercise machines from Russia since 2002. Ladies only health clubs under registered trade mark TONUS-CLUB. A unique format of Ladies-only boutique wellness studio, with a strictly individual training system based on personal characteristics and objectives. More than 150 clubs were opened in Europe and Asia.">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <meta name="wot-verification" content="3afe7e97744ee8b4e325"/>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+   
+    <?php wp_head(); ?>
+<script type="text/javascript" src="/wp-content/themes/entonus/assets/js/main.js"></script>
+
+<section class="partner-header-section">
+
+
+<header id="header" class="partner-header">
+	
+        <div class="container">
+            <div class="site-header">
+                <a href="/"><img class="logo-icon" src="/wp-content/themes/entonus/assets/images/logo-new.png" alt="icon"></a>
+            
+                        <!--//logo-->
+           
+            </div>
+
+<div class="partner-header-wrapper">
+
+<div class="partner-header-title">
+
+	<h1 class="partner-title">Ladies only health club</h1>
+	<p class="partner-subtitle">with smart exercise machines and a set of pleasant rejuvenating sessions</p>
+
+</div>
+</div>
+
+
+
+
+
+ </div><!--//container-->
+
+
+
+
+
+
+
+
+
+    </header><!--//header-->
+
+
+</section>
+
+
+
+
+
+<section class="item1">
+	
+	<div class="container">
+		<div class="row">
+			
+	
+		<div class="col-sm-12 col-md-6 col-lg-6">
+			<div class="left-item">
+			<h2 class="left-item-title">FITNESS CLUB <br>FOR LADIES</h2>
+	
+
+
+		<p class="left-item-subtitle">Our clubs are specially created so that each lady can take care of her figure and health in a comfortable environment near home, without men. We do not have a schedule like in ordinary fitness clubs, but there are unique training sessions that are individually compiled for each woman. No need to pay extra for an individual coach, everything is already included in the card! Our club is perfect for beginners in their fitness path, as all workouts are gentle, without stress on the heart, joints and spine.</p>
+	
+	</div>
+	</div>
+
+
+		
+			
+				<div class="col-sm-12 col-md-6 col-lg-6 d-flex align-items-center justify-content-center">
+					<div class="right-item">
+						<!-- <iframe src="https://www.youtube.com/watch?v=1AVdsGGJ17k" frameborder="0"></iframe> -->
+							<div class="play-btn-block">
+								<img src="/wp-content/themes/entonus/assets/images/page-partner/play-btn.png" alt="">
+							</div>
+	
+				</div>
+
+
+
+		</div>
+
+
+
+
+	</div>
+		</div>
+
+</section>
+
+
+
+
+
+
+
+<section class="item2">
+	
+<div class="item2-wrapper">
+
+
+
+<div class="container">
+					
+				<h2 class="item2-title">HOW WILL YOUR FIRST TRAINING GO?</h2>
+				<p class="item2-subtitle">smart training</p>
+
+<div class="item2-inner-wrapper">
+
+			<div class="row">
+
+
+					
+                  <div class="col-sm-12 col-md-6 col-lg-6">
+                  	<div class="train-item-wrapper d-flex flex-column  flex-md-row">
+                  	
+                  	<div class="train-item item-1-1 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+
+                  </div>
+
+
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+                    
+                    </div>
+                  	</div>
+
+
+                  	<div class="col-sm-12 col-md-6 col-lg-6">
+                  		<div class="train-item-wrapper train-item-wrapper-2 d-flex flex-column  flex-md-row">
+					<div class="train-item item-1-2 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+                    		</div>
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+					</div>
+                  	</div>
+
+			</div>
+
+
+			<div class="row">
+
+
+					
+                  <div class="col-sm-12 col-md-6 col-lg-6">
+                  	<div class="train-item-wrapper d-flex flex-column  flex-md-row">
+                  	
+                  	<div class="train-item item-1-3 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+
+                  </div>
+
+
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+                    
+                    </div>
+                  	</div>
+
+
+                  	<div class="col-sm-12 col-md-6 col-lg-6">
+                  		<div class="train-item-wrapper train-item-wrapper-2 d-flex flex-column  flex-md-row">
+					<div class="train-item item-1-4 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+                    		</div>
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+					</div>
+                  	</div>
+
+			</div>
+
+
+
+
+			<div class="row">
+
+
+					
+                  <div class="col-sm-12 col-md-6 col-lg-6">
+                  	<div class="train-item-wrapper d-flex flex-column  flex-md-row">
+                  	
+                  	<div class="train-item item-1-5 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+
+                  </div>
+
+
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+                    
+                    </div>
+                  	</div>
+
+
+                  	<div class="col-sm-12 col-md-6 col-lg-6">
+                  		<div class="train-item-wrapper train-item-wrapper-2 d-flex flex-column  flex-md-row">
+					<div class="train-item item-1-6 train-item-img">
+                  		
+                    <img src="/wp-content/themes/entonus/assets/images/page-partner/train-img-1.jpg" alt="">
+                    		</div>
+                    <div class="train-item train-item-text">
+                    <h4 class="train-item-title">CONSULTATION</h4>
+                    <p class="train-item-subtitle">Communication with a personal club trainer. 
+                    You will set a clear goal together and outline a plan to achieve it.</p>
+                    </div>
+					</div>
+                  	</div>
+
+			</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+</div>	
+
+</section>
+
+
+<section class="section-item3">
+	
+<div class="section-item3-background">
+	
+<div class="container">
+
+
+	<div class="mobile-img-owner d-block d-sm-none">
+	<img src="/wp-content/themes/entonus/assets/images/page-partner/owner-background.jpg" alt="">
+	</div>
+	
+	<h2 class="sec9-title">ФЕДОТЮК ИННА 
+
+СЕРГЕЕВНА</h2>
+<p class="item3-subtitle">Владелец фитнес-клуба на <br>
+ ул.Оптиков 45к1 в СС</p>
+
+
+ <p class="item3-subtitle-quote">
+ 	Training on toning tables are compared <br>to basic strength exercises, as in the gym,<br> but the speed of movement and amplitude<br> is set by the exercise machine itself <br>- it is mechanical.
+ </p>
+
+
+ <p class="item3-subtitle-quote">
+ 	Training on toning tables are compared <br>to basic strength exercises, as in the gym,<br> but the speed of movement and amplitude<br> is set by the exercise machine itself <br>- it is mechanical.
+ </p>
+</div>
+
+</div>
+
+
+
+</section>
+
+<section class="section-item4">
+	
+<div class="container">
+	
+<div class="section-item4-block">
+	
+	<h3 class="section-item4-title">WHY TONUS-CLUB IN ERBIL,<br> NOT ANY REGULAR FITNESS CLUB?</h3>
+
+	<h4 class="section-item4-subtitle">RIGHT CHOICE</h4>
+	<p class="section-item4-text">
+		TONUS-CLUB is a delicate work on improving your own body.<br> Gentle and effective. While training it is important to take <br>into account the woman’s body characteristics,<br> which means not forcing your body and putting pressure on it.
+	</p>
+</div>
+
+<div class="section-item4-features">
+	<p>- We work with ladies only and we understand all the features and subtleties of the female body by 100%.</p>
+	<p>- A good-natured family atmosphere that allows you to completely relax and take time for yourself.</p>
+	<p>- Constant feedback from clients allows us to improve and create the best fitness solutions for ladies in Erbil.</p>
+	<p>- Correct ideals. You choose your landmark yourself and move towards it at a comfortable pace.</p>
+	<p>- Full care and responsibility for your result</p>
+	<div class="sign-up-link">
+	<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+</div>
+
+
+
+
+
+</div>
+
+
+</section>
+
+
+
+
+
+
+
+
+
+<section class="section-item5">
+<div class="container">
+
+
+<div class="partner-products">
+	
+<h2 class="partner-products-title">EXERCISE MACHINES THANKS TO WHICH <br>YOU WILL GET THE DESIRED RESULT</h2>
+
+
+
+						<div class="partner-products-mobile d-block d-sm-none">
+
+						<div class="slider-desc">
+						<h4>Баротренажер</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+
+						</div>
+						</div>
+						
+
+
+
+
+
+<div class="partner-products-slider">
+	
+	<!--
+We will make a slider with stylized thumbnails using CSS3
+The markup is very simple:
+Radio Inputs
+Labels with thumbnails to detect click event
+Main Image
+-->
+<div class="slider">
+						<input type="radio" name="slide_switch" id="id1"/>
+						<label for="id1">
+							<img src="/wp-content/themes/entonus/assets/images/prod/ts1-550.png" width="100"/>
+							
+						</label>
+						<img src="/wp-content/themes/entonus/assets/images/prod/ts1-550.png"/>
+						<div class="slider-desc">
+						<h4>Баротренажер 1</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+
+						</div>
+						
+						<!--Lets show the second image by default on page load-->
+						<input type="radio" name="slide_switch" id="id2" checked="checked"/>
+						<label for="id2">
+							<img src="/wp-content/themes/entonus/assets/images/prod/ts2-550.png" width="100"/>
+
+						</label>
+						<img src="/wp-content/themes/entonus/assets/images/prod/ts2-550.png"/>
+						<div class="slider-desc">
+						<h4>Баротренажер</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+
+						</div>
+						
+						<input type="radio" name="slide_switch" id="id3"/>
+						<label for="id3">
+							<img src="/wp-content/themes/entonus/assets/images/prod/ts3-550.png" width="100"/>
+						</label>
+						<img src="/wp-content/themes/entonus/assets/images/prod/ts3-550.png"/>
+						<div class="slider-desc">
+						<h4>Баротренажер</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100 " href="">sign up</a></div>
+
+						</div>
+						
+						<input type="radio" name="slide_switch" id="id4"/>
+						<label for="id4">
+							<img src="/wp-content/themes/entonus/assets/images/prod/ts4-550.png" width="100"/>
+						</label>
+						<img src="/wp-content/themes/entonus/assets/images/prod/ts4-550.png"/>
+						<div class="slider-desc">
+						<h4>Баротренажер</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+
+						</div>
+						
+						<input type="radio" name="slide_switch" id="id5"/>
+						<label for="id5">
+							<img src="/wp-content/themes/entonus/assets/images/prod/ts5-550.png" width="100"/>
+						</label>
+						<img src="/wp-content/themes/entonus/assets/images/prod/ts5-550.png"/>
+						<div class="slider-desc">
+						<h4>Баротренажер</h4>
+						<p>Exclusive Treadmill Machine is good for that people, <br>	 who want to make faster the body fat burning <br> in their “problem areas”.</p>
+
+						<div class="sign-up-link">
+						<a class="partners-sign-up popmake-1100" href="">sign up</a></div>
+
+						</div>
+</div>
+
+
+
+<!-- We will use PrefixFree - a script that takes care of CSS3 vendor prefixes
+You can download it from http://leaverou.github.com/prefixfree/ -->
+<!-- <script src="https://raw.githubusercontent.com/LeaVerou/prefixfree/master/prefixfree.min.js" type="text/javascript"></script> -->
+
+
+</div>
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+</section>
+
+
+
+
+
+<section class="section-item6">
+
+     <div id="success-stories" class="success-gallery">
+
+	<h2 class="section-title">Gallery</h2>
+	<div class="space-gallery"></div>
+
+	<?php echo do_shortcode( '[the_grid name="clubs-gal"]' ); ?>
+</div>
+
+
+
+
+
+
+
+
+</section>
+
+<section class="section-item7">
+
+<div class="container">
+	<div class="sign-up-footer">
+
+
+	<h2>Are you ready to try?</h2>
+	<p>Sign up for a free trial session with a personal coach</p>
+	<p><s>200$</s><span>0$</span></p>
+
+	<div class="sign-up-footer">
+	<a class="popmake-1100" href="">sign up</a>		
+	</div>
+
+	</div>
+</div>
+
+
+</section>
+
+
+
+<section class="section-item8">
+
+<div id="find-club" class="full-map">
+	<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A37d66d51fe02c7a8fae0fe30639daefe9663ee8551f28244db8411bf6b0e97f0&amp;width=100%25&amp;height=auto&amp;lang=ru_RU&amp;scroll=true">
+		map.events.once('click', function () {
+	map.behaviors
+		.disable('scrollZoom')
+		.disable('multiTouch');
+	});
+	</script>
+	<div id="map">
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_P9S2n_FiNoIlYSfzWfaOgiiMQrz1Er0&callback=initMap">
+		function initMap() {
+    	var coordinates = {lat: 47.212325, lng: 38.933663},
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: coordinates
+						scrollwheel: boolean
+        });
+			}
+		</script>
+	</div>
+</div>
+</section>
+
+
+
+
+
+<section class="section-item9">
+
+<?php add_revslider('nice-and-clean-footer-1'); ?>
+</section>
+
+
+<?php wp_footer(); ?>
